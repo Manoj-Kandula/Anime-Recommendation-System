f36223cb3e0b5a04d13db84f5a6bfcabb104a4b0
Manoj Kandula <dr.dragonblade9@gmail.com>
2025-03-17 21:08:51 +0530

    Updated Model

--- Anime-web-scrap/src/Model/util.py
+++ Anime-web-scrap/src/Model/util.py
@@ -1,16 +1,38 @@
-import pickle,sys
-with open('Model/anime_Recommender.pickle','rb') as file:
-    data = pickle.load(file)
-    knn = data['knn']
-    features_scaled = data['features_scaled']
-    data1 = data['data1']
-def recommend_anime(anime_name):
-    anime_id = data1[data1['name'] == anime_name].iloc[0, 0]
-    anime_features = features_scaled[anime_id - 1].reshape(1, -1)
-    distances, indices = knn.kneighbors(anime_features)
-    similar_anime_ids = indices.flatten()[1:]
-    similar_anime = data1.loc[similar_anime_ids,'name']
-    similar_anime = similar_anime.tolist()
-    print(similar_anime)
-n = sys.argv[1]
-recommend_anime(n)
+import pickle, sys
+from sklearn.metrics.pairwise import cosine_similarity
+
+def recommend_anime_by_name(anime_name):
+    with open('models.pkl', 'rb') as f:
+        models = pickle.load(f)
+    
+    mlb = models['mlb']
+    svd = models['svd']
+    scaler = models['scaler']
+    data1 = models['data']
+    
+    genre_matrix = mlb.transform(data1['genre'])
+    genre_svd = svd.transform(genre_matrix)
+    
+    genre_similarity = cosine_similarity(genre_svd)
+    
+    views_normalized = scaler.transform(data1[['members']])
+    
+    anime_index = data1[data1['name'] == anime_name].index[0]
+    
+    genre_similarities = genre_similarity[anime_index]
+    views_similarity = views_normalized.flatten()
+    
+    combined_similarity = 0.9 * genre_similarities + 0.1 * views_similarity
+    
+    similar_indices = combined_similarity.argsort()[::-1][1:11]
+    recommended_animes = data1.iloc[similar_indices]
+    ans = []
+    for i in recommended_animes['name']:
+        if i != anime_name:
+            ans.append(i)
+        else:
+            continue
+    return ans
+anime_name = sys.argv[1]
+recommended_animes = recommend_anime_by_name(anime_name)
+print(recommended_animes)
